1e81f30b79d737919773d7bdf38bbffae2e71440
ACOLORBLUE <info@acolorblue.co>
2018-09-08 00:06:28 -0700

    Update local.js

--- local.js
+++ local.js
@@ -8,6 +8,11 @@ function clockZoom() {
     .on('mouseleave', function() {
     $(this).parents('clock').removeClass('zoomed');
   })
+  
+  
+  // $('.world-clock .timezones clock').on('mousemove', function(e){
+  //   $(this).children('.border').css({'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()) * 100 +'%'});
+// })
 }
 
   
