c13aa91342be4c548a589083a5aaec6bfab047a0
ACOLORBLUE <info@acolorblue.co>
2018-11-08 19:01:26 -0800

    Update local.js

--- local.js
+++ local.js
@@ -1,44 +1,9 @@
-// REMOVE LOADER
-function loaderRandomColor() {
-  setTimeout(function() {
-    indicatorsContainer();
-  }, 2000);
-  
-  setTimeout(function() {
-    $('.loader').addClass('hide');
-  }, 3000);
-  
-  setTimeout(function() {
-    $('.loader').remove();
-    window.clearInterval(random_color_generator_interval);
-  }, 3400);
-}
-
-
-
-
-// CLOCK ZOOM
-function clockZoom() {
-  $('.computer .world-clock .timezones clock .numeric-indicators')
-    .on('mouseover', function() {
-    $(this).parents('clock').addClass('zoomed');
-  })
-
-    .on('mouseleave', function() {
-    $(this).parents('clock').removeClass('zoomed');
-  })
-  
-  // $('.world-clock .timezones clock').on('mousemove', function(e){
-  //   $(this).children('.border').css({'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()) * 100 +'%'});
-// })
-}
-
-  
-
-
 // WINDOW ON LOAD
 window.onload = function() {
-  randomColorGeneratorContainer();
-  loaderRandomColor();
-  clockZoom();
+  monotoneBreather('.loader', 2000);
+  removeLoader('.loader', 'no-opacity', 9000, 9400);
+  setTimeout(function() {
+    indicatorsContainer();
+  }, 8400);
+  computerImageZoom('.computer .world-clock .timezones clock .border');
 }
